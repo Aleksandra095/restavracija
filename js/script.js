f7db74ef92033261854d58c9444ae1bb5425ac2f
Aleksandra095 <92690797+Aleksandra095@users.noreply.github.com>
2026-07-08 22:49:22 +0300

    commit2
    commit2

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ menu.style.display="block";
 }
 
 
-
 let count=document.getElementById("count");
 
 let service=document.getElementById("service");
@@ -28,25 +27,20 @@ Number(service.value);
 let quantity=
 Number(count.value);
 
-
 total.innerHTML=
 price*quantity;
 
 }
-
 
 count.addEventListener(
 "input",
 calculate
 );
 
-
 service.addEventListener(
 "change",
 calculate
 );
-
-
 
 document
 .getElementById("form")
